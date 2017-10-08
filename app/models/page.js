84921d640769334a8b03a3f84943c07ade989580
Warren Clough <clough.warren@gmail.com>
2017-10-08 10:44:48 -0500

    updated site api

---
+++ app/models/page.js
@@ -0,0 +1,42 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let PageSchema = new Schema({
+    title: String,
+    homePage: Boolean,
+    slug: {
+        type: String,
+        index: { unique: true }
+    },
+    header: new Schema({
+        type: {
+            type: String,
+            required: true
+        },
+        heading: {
+            type: String,
+            required: false
+        },
+        messages: {
+            type: [String],
+            required: false,
+        },
+        imageUrl: {
+            type: String,
+            required: false,
+        }
+    }, { strict: false }),
+    content: new Schema({
+
+    }, { strict: false }),
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+module.exports = mongoose.model('Page', PageSchema);
