7b413b45a7a3ad8d240d5c25f6f061e361fe66e0
Warren Clough <clough.warren@gmail.com>
2017-12-04 13:48:36 -0500

    added back previous db schema for page

--- model/page/schema.js
+++ model/page/schema.js
@@ -1,11 +1,43 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
 const pageSchema = new Schema({
-  title: { type: String, required: true },
-  body: { type: String }
+  title: String,
+  homePage: Boolean,
+  slug: {
+    type: String,
+    index: { unique: true }
+  },
+  header: new Schema({
+    type: {
+      type: String,
+      required: true
+    },
+    heading: {
+      type: String,
+      required: false
+    },
+    message: {
+      type: String,
+      required: false
+    },
+    imageUrl: {
+      type: String,
+      required: false
+    }
+  }, { strict: false }),
+  content: new Schema({
+
+  }, { strict: false }),
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  updated: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 
-module.exports =  pageSchema;
+module.exports = pageSchema;
